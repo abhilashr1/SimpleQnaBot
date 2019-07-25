d646ad64daff9b228b192cf9133d0d478e3bee8b
Abhilash Rejanair <abrejana@microsoft.com>
2019-07-25 20:32:24 +0530

    Initial Commit

---
+++ Bots/SimpleQnaBot.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class SimpleQnaBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("qa.json")
+                        .Build();
+
+            string question = turnContext.Activity.Text.ToLower().Trim();
+            string answer = config.GetValue<string>(question);
+
+            if (!string.IsNullOrEmpty(answer))
+                await turnContext.SendActivityAsync(MessageFactory.Text($"{answer}"), cancellationToken);
+            else
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Sorry, I could not find an answer to that question."), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to the bot! Please type your question. "), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+﻿# SimpleQna Bot
+
+Azure QnaMaker is a little expensive if you want a simple qna bot with exact-matching questions (if not, use qnamaker or something similar). 
+
+## Prerequisites
+
+- [.NET Core SDK](https://dotnet.microsoft.com/download) version 2.1
+
+  ```bash
+  # determine dotnet version
+  dotnet --version
+  ```
+
+## To try this sample
+
+- Clone the repository
+
+- In a terminal, navigate to `samples/csharp_dotnetcore/02.echo-bot`
+- Run the bot from a terminal or from Visual Studio, choose option A or B.
+
+  A) From a terminal
+
+  ```bash
+  # run the bot
+  dotnet run
+  ```
+
+  B) Or from Visual Studio
+
+  - Launch Visual Studio
+  - File -> Open -> Project/Solution
+  - Press `F5` to run the project
+
+## Testing the bot using Bot Framework Emulator
+
+[Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
+
+- Install the Bot Framework Emulator version 4.3.0 or greater from [here](https://github.com/Microsoft/BotFramework-Emulator/releases)
+
+### Connect to the bot using Bot Framework Emulator
+
+- Launch Bot Framework Emulator
+- File -> Open Bot
+- Enter a Bot URL of `http://localhost:3978/api/messages`
+
+## Deploy the bot to Azure
+
+To learn more about deploying a bot to Azure, see [Deploy your bot to Azure](https://aka.ms/azuredeployment) for a complete list of deployment instructions.
+
+## Further
+
+Pull requests gladly accepted.
